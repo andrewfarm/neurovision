07c95ae83645ed525566ebe754bb344d49c7db8f
andrewfarm <Andrew.Farm@tufts.edu>
2018-05-15 16:38:25 -0400

    backprop & training (slows browser)

--- network.js
+++ network.js
@@ -80,7 +80,7 @@ function randomMatrix(n, m) {
     for (var i = 0; i < n; i++) {
         result[i] = new Array(m);
         for (var j = 0; j < m; j++) {
-            result[i][j] = Math.random() * 2.0 - 1.0;
+            result[i][j] = 0.0;//Math.random() * 2.0 - 1.0;
         }
     }
     return result;
@@ -165,7 +165,7 @@ function loadData(dataString) {
         }
     }
     
-    shuffle(data);
+//    shuffle(data);
     return {
         trainingData:   data.slice(0, Math.floor(data.length * 0.6)),
         validationData: data.slice(Math.floor(data.length * 0.6), Math.floor(data.length * 0.8)),
@@ -187,6 +187,21 @@ function sigmoidPrime(p) {
     return sigmoid_p * (1.0 - sigmoid_p);
 }
 
+/* Returns the index of the largest component in a (row-major) vector.
+ * v must have at least one compoenent. */
+function maxComponent(v) {
+    var indexOfMax = 0;
+    var max = v[0][0];
+    for (var i = 1; i < v.length; i++) {
+        var value = v[i][0];
+        if (value > max) {
+            max = value;
+            indexOfMax = i;
+        }
+    }
+    return indexOfMax;
+}
+
 class Network {
     
     constructor(sizes) {
@@ -206,12 +221,113 @@ class Network {
     }
     
     /* Feeds the input vector through the network and returns the vector of
-     * output activations */
+     output activations */
     feedForward(input) {
         var output = input;
         for (var i = 0; i < this.numLayers - 1; i++) {
             output = apply(sigmoid, add(mul(this.weights[i], output), this.biases[i]));
         }
         return output;
+    }
+    
+    /* Performs backpropagation and returns an object {changeWeights,
+     changeBiases} where changeWeights is a list describing the amount to
+     change the weights of each layer (the rate of change of the error, not
+     including the learning rate) and changeBiases is similarly for the
+     biases */
+    backprop(input, expectedOutput) {
+        // forward propagation
+        // like feedForward(), but potentials and outputs for each neuron are saved
+        var potentials = []; // list of vectors of potentials for each layer
+        var outputs = [input]; // list of vectors of output activations for each layer
+        var output = input;
+        for (var i = 0; i < this.numLayers - 1; i++) {
+            var potentialVector = add(mul(this.weights[i], output), this.biases[i]);
+            potentials.push(potentialVector);
+            output = apply(sigmoid, potentialVector);
+            outputs.push(output);
+        }
+        
+        // backward propagation
+        
+        var changeWeights = new Array(this.numLayers - 1);
+        var changeBiases  = new Array(this.numLayers - 1);
+        
+        // calculation of error
+        var delta = mulElementwise(apply(sigmoidPrime, potentials[potentials.length - 1]), sub(expectedOutput, output));
+        
+        // rate of change of the error (not including the learning rate)
+        changeWeights[changeWeights.length - 1] = mul(delta, transpose(outputs[outputs.length - 2]));
+        changeBiases[changeBiases.length - 1] = delta; // bias neuron has an implicit activation of 1.0, so there's no need to multiply
+        
+        for (var l = 2; l < this.numLayers; l++) {
+            // contribution to the error by hidden neurons
+            delta = mulElementwise(apply(sigmoidPrime, potentials[potentials.length - l]), mul(transpose(this.weights[this.weights.length - l + 1]), delta));
+            
+            // rate of change of the error (not including the learning rate)
+            changeWeights[changeWeights.length - l] = mul(delta, transpose(outputs[outputs.length - l - 1]));
+            changeBiases[changeBiases.length - l] = delta; // bias neuron has an implicit activation of 1.0, so there's no need to multiply
+        }
+        
+        return {changeWeights: changeWeights, changeBiases: changeBiases};
+    }
+    
+    /* Performs backpropagation and updates the weights and biases according to
+     the learning rate eta */
+    backpropAndUpdate(input, expectedOutput, eta) {
+        var backpropResults = this.backprop(input, expectedOutput);
+        for (var l = 0; l < this.numLayers - 1; l++) {
+            // update weights and biases according to eta and rates of change
+            this.weights[l] = add(this.weights[l], apply(function (w) { return eta * w; }, backpropResults.changeWeights[l]));
+            this.biases[l] = add(this.biases[l], apply(function (b) { return eta * b; }, backpropResults.changeBiases[l]));
+        }
+    }
+    
+    /* Trains the network on trainingData, printing accuracy results on
+     validationData, until either the average total error over all the
+     validationData is below targetError (default 0.02) or the average total
+     error increases by a factor of maxFluctuation (default 0.7) over the
+     minimum average total error so far */
+    train(trainingData, validationData, eta, targetError=0.02, maxFluctuation=0.7) {
+        var avgTotalError = Infinity;
+        var minAvgTotalError = Infinity;
+        
+        for (var epoch = 0; epoch < 10000; epoch++) {
+            if (avgTotalError < targetError) {
+                console.log("Training stopped (avg. total error below " + targetError + ")");
+                return;
+            }
+            if ((avgTotalError / minAvgTotalError) > (1.0 + maxFluctuation)) {
+                console.log("Training stopped (avg. total error beginning to increase)");
+                return;
+            }
+            
+            // run backpropagation 10 times before checking validation data
+            for (var i = 0; i < 10; i++) {
+//                var example = trainingData[Math.floor(Math.random() * trainingData.length)];
+                var example = trainingData[(epoch * 10 + i) % trainingData.length];
+                this.backpropAndUpdate(example.features, example.label, eta);
+            }
+            
+            // calculate accuracy on validation data
+            var numCorrect = 0;
+            var avgTotalError = 0;
+            for (var v of validationData) {
+                var vOutput = this.feedForward(v.features);
+                if (maxComponent(v.label) == maxComponent(vOutput)) {
+                    numCorrect += 1;
+                }
+                for (var i = 0; i < vOutput.length; i++) {
+                    avgTotalError += 0.5 * Math.pow(v.label[i][0] - vOutput[i][0], 2);
+                }
+            }
+            avgTotalError /= validationData.length;
+            if (avgTotalError < minAvgTotalError) {
+                minAvgTotalError = avgTotalError;
+            }
+            console.log("Epoch " + epoch + ": " + Math.floor(numCorrect * 100 / validationData.length) + "% accurate on validation set, avg. total error = " + avgTotalError);
+        }
+        
+        console.log("Training stopped (max iterations reached)");
     }
 }
