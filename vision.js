7a9dda09ae88a01808b7903995eab95bdca3a2f9
andrewfarm <Andrew.Farm@tufts.edu>
2018-05-15 23:15:28 -0400

    render quad with feedforward (not quite working)

--- vision.js
+++ vision.js
@@ -18,12 +18,42 @@ const QUAD_FRAG = '\
 \n\
 precision mediump float;\n\
 \n\
+uniform mat2x3 u_w0;\n\
+uniform vec3 u_b0;\n\
+\n\
+uniform mat3x3 u_w1;\n\
+uniform vec3 u_b1;\n\
+\n\
+vec3 sigmoid(vec3 v) {\n\
+    return vec3(1.0) / (vec3(1.0) + exp(-v));\n\
+}\n\
+\n\
+vec3 feedForward(vec2 features) {\n\
+    vec3 l1 = sigmoid(u_w0 * features + u_b0);\n\
+    vec3 l2 = sigmoid(u_w1 * l1 + u_b1);\n\
+    return l2;\n\
+}\n\
+\n\
+vec3 classColor(vec3 labelVector) {\n\
+//    return vec3(\n\
+//            1.0 - labelVector.z,\n\
+//            1.0 - labelVector.x,\n\
+//            1.0 - labelVector.y);\n\
+//    return vec3(\n\
+//            1.0 - min(ceil(labelVector.z - labelVector.x), ceil(labelVector.z - labelVector.y)),\n\
+//            1.0 - min(ceil(labelVector.x - labelVector.y), ceil(labelVector.x - labelVector.z)),\n\
+//            1.0 - min(ceil(labelVector.y - labelVector.x), ceil(labelVector.y - labelVector.z)));\n\
+    return ((labelVector.x > labelVector.y) && (labelVector.x > labelVector.z)) ?\n\
+            vec3(1.0, 0.0, 1.0) :\n\
+            ((labelVector.y > labelVector.z) ? vec3(1.0, 1.0, 0.0) : vec3(0.0, 1.0, 1.0));\n\
+}\n\
+\n\
 in vec2 v_pos;\n\
 \n\
 out vec4 color;\n\
 \n\
 void main() {\n\
-    color = vec4(v_pos.x, v_pos.y, 0.0, 1.0);\n\
+    color = vec4(classColor(feedForward(v_pos)), 1.0);\n\
 }\n\
 ';
 
@@ -64,6 +94,33 @@ void main() {\n\
 }\n\
 ';
 
+const EXAMPLES_OUTLINE_VERT = '\
+#version 300 es\n\
+\n\
+precision mediump float;\n\
+\n\
+in vec2 a_pos;\n\
+in float a_class;\n\
+\n\
+void main() {\n\
+    float dummy = a_class;\n\
+    gl_Position = vec4(a_pos * 2.0 - vec2(1.0, 1.0), 0.0, 1.0);\n\
+    gl_PointSize = 12.0;\n\
+}\n\
+';
+
+const EXAMPLES_OUTLINE_FRAG = '\
+#version 300 es\n\
+\n\
+precision mediump float;\n\
+\n\
+out vec4 color;\n\
+\n\
+void main() {\n\
+    color = vec4(0.0, 0.0, 0.0, 1.0);\n\
+}\n\
+';
+
 var canvas = document.getElementById("canvas");
 
 const gl = canvas.getContext("webgl2");
@@ -77,12 +134,19 @@ var examplesPosBuffer;
 var examplesClassBuffer;
 var examplesVAO;
 var examplesShaderProgram;
+var examplesOutlineShaderProgram;
+
+var w0 = new Float32Array(6);
+var b0 = new Float32Array(3);
+var w1 = new Float32Array(9);
+var b1 = new Float32Array(3);
 
 if (gl) {
     gl.viewport(0, 0, canvas.width, canvas.height);
     
     quadShaderProgram = createProgram(gl, QUAD_VERT, QUAD_FRAG);
     examplesShaderProgram = createProgram(gl, EXAMPLES_VERT, EXAMPLES_FRAG);
+    examplesOutlineShaderProgram = createProgram(gl, EXAMPLES_OUTLINE_VERT, EXAMPLES_OUTLINE_FRAG);
     
     quadPosBuffer = createBuffer(gl, new Float32Array(
             [-1, -1, 1, -1, -1, 1, -1, 1, 1, -1, 1, 1]));
@@ -118,13 +182,41 @@ function setExamples(trainingData) {
     gl.bindVertexArray(null);
 }
 
+function convertToGlMatrix(a, m) {
+    for (var i = 0; i < m.length; i++) {
+        for (var j = 0; j < m[0].length; j++) {
+            a[j * m.length + i] = m[i][j];
+        }
+    }
+}
+
+function convertToGlVector(a, m) {
+    for (var i = 0; i < m.length; i++) {
+        a[i] = m[i][0];
+    }
+}
+
+function setWeights(net) {
+    convertToGlMatrix(w0, net.weights[0]);
+    convertToGlVector(b0, net.biases[0]);
+    convertToGlMatrix(w1, net.weights[1]);
+    convertToGlVector(b1, net.weights[1]);
+}
+
 function render() {
     gl.useProgram(quadShaderProgram.programID);
     gl.bindVertexArray(quadVAO);
+    //the matrices and vectors in the shaders are transposed, so transpose is set to true
+    gl.uniformMatrix2x3fv(quadShaderProgram.u_w0, false, w0);
+    gl.uniform3fv(quadShaderProgram.u_b0, b0);
+    gl.uniformMatrix3fv(quadShaderProgram.u_w1, false, w1);
+    gl.uniform3fv(quadShaderProgram.u_b1, b1);
     gl.drawArrays(gl.TRIANGLES, 0, 6);
     
-    gl.useProgram(examplesShaderProgram.programID);
     gl.bindVertexArray(examplesVAO);
+    gl.useProgram(examplesOutlineShaderProgram.programID);
+    gl.drawArrays(gl.POINTS, 0, numExamples);
+    gl.useProgram(examplesShaderProgram.programID);
     gl.drawArrays(gl.POINTS, 0, numExamples);
 }
 
